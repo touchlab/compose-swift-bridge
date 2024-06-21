294659db43ab78715e5b9857125783c46ce25478
Gabriel Souza <devsrsouza@gmail.com>
2024-06-21 17:35:15 -0300

    Fix UiViewController sample

--- sample/iosApp/iosApp/NativeViewFactory.swift
+++ sample/iosApp/iosApp/NativeViewFactory.swift
@@ -1,20 +1,11 @@
 import SwiftUI
 import ComposeApp
 
-class TestDelegate1 : MapViewDelegate {
-    func updateCoordinate(coordinate: MapCoordinates) {
-        
-    }
-    
-    func updateTitle(title: String) {
-        
-    }
-}
-
 class SwiftUINativeViewFactory : NativeViewFactory {
     func createMapView(coordinate: MapCoordinates, title: String) -> (view: UIViewController, delegate: any MapViewDelegate) {
-        let view = NativeMapView(title: title, coordinate: coordinate)
-        return (view: UIHostingController(rootView: view), delegate: TestDelegate1())
+        let observable = MapViewObservable(coordinate: coordinate, title: title)
+        let view = NativeMapViewBinding(observable: observable)
+        return (view: UIHostingController(rootView: view), delegate: observable)
     }
     
     func createMapViewWithUiView(coordinate: MapCoordinates, title: String) -> (view: UIView, delegate: any MapViewWithUiViewDelegate) {
